28e9b15ad582296de4db5c002604760572b167f8
Mu <bulzmu98@gmail.com>
2026-02-02 15:51:07 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -69,7 +69,7 @@ readp "请输入username：" USERNAME
 readp "请输入password：" PASSWORD
 TOKEN="${USERNAME}${PASSWORD}"
 purple "TOKEN：$TOKEN"
-while true; do; readp "请确认令牌[Yes/No]：\n" INPUT; case $INPUT in; [yY][eE][sS]|[yY]) purple "已确认。"; break;; [nN][oO]|[nN]) blue "请重新输入。"; readp "请输入username：\n" USERNAME; readp "请输入password：\n" PASSWORD; TOKEN="${USERNAME}${PASSWORD}"; purple "TOKEN：$TOKEN";; *) red "错误，请重新输入！"; continue;; esac; done
+while true; do readp "请确认令牌[Yes/No]：\n" INPUT; case $INPUT in [yY][eE][sS]|[yY]) purple "已确认。"; break;; [nN][oO]|[nN]) blue "请重新输入。"; readp "请输入username：\n" USERNAME; readp "请输入password：\n" PASSWORD; TOKEN="${USERNAME}${PASSWORD}"; purple "TOKEN：$TOKEN";; *) red "错误，请重新输入！"; continue;; esac done
 }
 
 # Nginx
