ac6bc2db400667c5f7b61e45fb2407fc066ff41f
Mu <bulzmu98@gmail.com>
2026-02-12 11:49:03 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -420,12 +420,14 @@ XRAY
 }
 
 sh_apt(){
-  blue "开始安装。"
-  curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor | tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
-  #gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
-  echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] https://nginx.org/packages/ubuntu `lsb_release -cs` nginx" | tee /etc/apt/sources.list.d/nginx.list
-  echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" | tee /etc/apt/preferences.d/99nginx
-  apt-get update && apt install -y unzip certbot nginx
+  if [ ! -s /etc/apt/preferences.d/99nginx ]; then
+    blue "开始安装。"
+	curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor | tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+	#gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
+	echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] https://nginx.org/packages/ubuntu `lsb_release -cs` nginx" | tee /etc/apt/sources.list.d/nginx.list
+	echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" | tee /etc/apt/preferences.d/99nginx
+	apt-get update && apt install -y unzip certbot nginx
+  fi
 }
 
 sh_domain(){
