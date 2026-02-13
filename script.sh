6f49fdf12fdbc692d12909f471e7c7421592f7cb
Mu <bulzmu98@gmail.com>
2026-02-13 08:36:52 +0800

    更新 script.sh

--- script.sh
+++ script.sh
@@ -431,7 +431,9 @@ sh_apt(){
 	#gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
 	echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] https://nginx.org/packages/ubuntu `lsb_release -cs` nginx" | tee /etc/apt/sources.list.d/nginx.list
 	echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" | tee /etc/apt/preferences.d/99nginx
-	apt-get update && apt install -y unzip certbot nginx
+  fi
+  if [ ! type nginx || ! type certbot || ! type unzip || ! type ufw ] >/dev/null 2>&1; then
+  apt-get update && apt install -y ufw unzip certbot nginx
   fi
 }
 
