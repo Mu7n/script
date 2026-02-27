0db000c8c56d3d8f38b30e19bae2f2ac6d3b0861
Mu <bulzmu98@gmail.com>
2026-02-27 12:17:31 +0800

    Suppress output for nginx key and source list commands
    Redirect output of commands to /dev/null to suppress output.

--- script.sh
+++ script.sh
@@ -521,8 +521,8 @@ sh_menuxray(){
 if [ ! -s /etc/apt/preferences.d/99nginx ]; then
   curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor | tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
   #gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
-  echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] https://nginx.org/packages/ubuntu `lsb_release -cs` nginx" | tee /etc/apt/sources.list.d/nginx.list
-  echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" | tee /etc/apt/preferences.d/99nginx
+  echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] https://nginx.org/packages/ubuntu `lsb_release -cs` nginx" | tee /etc/apt/sources.list.d/nginx.list >/dev/null
+  echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" | tee /etc/apt/preferences.d/99nginx >/dev/null
   sh_html
 fi
 if ! type "nginx" "certbot" "unzip" "ufw" >/dev/null 2>&1; then
